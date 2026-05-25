8f3eee2c45a8e5e756fe23d9bdb10d1d16bd5b83
Synergy Hub Africa <synergyhubafrica@gmail.com>
2026-05-26 01:28:44 +0300

    Use PORT env var for Render deployment, disable debug in production

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
@@ -49,4 +50,5 @@ def send_message():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
